9c60364c002d60a20a14affe201bc4015d491260
sarang <sarang.office01@gmail.com>
2025-11-07 15:05:17 +0530

    make changes

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -5,35 +5,22 @@ export async function POST(req) {
   try {
     const evt = await verifyWebhook(req);
 
-    const SIGNING_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
-    console.log(SIGNING_SECRET);
-    if (!SIGNING_SECRET) {
-      throw new Error("Error!! Please add SIGNING_SECRET from clerk");
-    }
-
     // Do something with payload
     // For this guide, log payload to console
     const { id } = evt.data;
     const eventType = evt.type;
 
     if (evt.type === "user.created") {
-      console.log("New user created:", evt.data.id);
-      // Handle user creation
+      console.log("userId:", evt.data.id);
     }
 
     if (evt.type === "user.updated") {
-      console.log("New user Updated:", evt.data.id);
-      // Handle user creation
+      console.log("userId:", evt.data.id);
     }
 
     if (evt.type === "user.deleted") {
-      console.log("New user Deleted:", evt.data.id);
-      // Handle user creation
+      console.log("userId:", evt.data.id);
     }
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`
-    );
-    console.log("Webhook payload:", evt.data);
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
