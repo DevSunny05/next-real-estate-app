8063f21a8e7d5b5c9e02921a30d2ed0688961634
sarang <sarang.office01@gmail.com>
2025-11-08 13:07:14 +0530

    make small changes

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,42 +1,40 @@
 import mongoose from "mongoose";
 
-// let initialized = false;
-// export const connect = async () => {
-//   mongoose.set("strictQuery", true);
+let initialized = false;
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
 
-//   if (initialized) {
-//     console.log("Mongodb already connected");
-//     return;
-//   }
-
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL, {
-//       dbname: "next-estate",
-//       useNewUrlParser: true,
-//       useUnifiedTopilogy: true,
-//     });
-
-//     initialized = trueconsole.log("Mongodb connected");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-export async function connect() {
-  try {
-    // Avoid deprecation warnings for query filters in older codebases
-    mongoose.set("strictQuery", false);
-
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-
-    console.log(
-      `MongoDB connected: ${conn.connection.host}:${conn.connection.port}/${conn.connection.name}`
-    );
-    return conn;
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    throw error;
+  if (initialized) {
+    console.log("Mongodb already connected");
+    return;
   }
-}
 
-export default connect;
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      dbname: "next-estate",
+      useNewUrlParser: true,
+      useUnifiedTopilogy: true,
+    });
+
+    initialized = trueconsole.log("Mongodb connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// export async function connect() {
+//   try {
+//     // Avoid deprecation warnings for query filters in older codebases
+//     mongoose.set("strictQuery", false);
+
+//     const conn = await mongoose.connect(process.env.MONGO_URL);
+
+//     console.log(`MongoDB connected`);
+//     return conn;
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//     throw error;
+//   }
+// }
+
+// export default connect;
